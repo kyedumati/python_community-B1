09437b22dbf91f207b032a7c4775c95924287ef4
kasiy <kasi.yedumati@gmail.com>
2022-01-25 19:03:32 +0530

    assessment1 code

---
+++ Assessments/Assessment1.py
@@ -0,0 +1,60 @@
+# i = 1
+# while True:
+#     if i%3 == 0:
+#         break
+#     print(i)
+#     i+=1
+
+
+# x = 'abcd'
+# for i in x:
+#     print(i.upper())
+
+
+# i=0
+# while i<5:
+#     print(i)
+#     i+=1
+#     if i==3:
+#         break
+# else:
+#     print(0)
+
+
+#
+# def addItem(listParam):
+#     listParam+=[1]
+#
+# mylist = [1,2,3,4]
+# addItem(mylist)
+# print(len(mylist))
+
+
+#
+# list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# print(list[1][2])
+#
+# def sum():
+#     print("inside funcition")
+#
+# for i in range(10):
+#     if i==5:
+#         break
+#     else:
+#         print(i)
+#         sum()
+# else:
+#     print("here")
+
+# List  = []
+# set  = {}
+# dict = {}
+# tuple = ()
+
+# print(id(mylist))
+# for i in range(2.0): print(i)
+
+
+# print("abcdef".find("cd"))
+# print("cd" in "abcdef")
+# print("abcdef".find("cd") == "cd" in "abcdef")
